97aa3f6dc9e486c99960c02649bf8098e6f75b2f
Paul Hebert <paul@cloudfour.com>
2019-11-11 13:55:27 -0800

    Create new `removeExamples` task

--- gulpfile.js/tasks/removeExamples.js
+++ gulpfile.js/tasks/removeExamples.js
@@ -1,12 +1,31 @@
 const gulp = require('gulp');
 const del = require('del');
-const fg = require('fast-glob');
-
-
+const replace = require('replace-in-file');
+/**
+ * A task to remove our example pattern and prototype.
+ * This can be run from the command line with `npx gulp removeExamples`
+ * This should not be run if you've already been making your own modifications,
+ * as this may interfere with them.
+ */
 gulp.task('removeExamples', async cb => {
-  console.log( fg.sync('src/@(patterns|prototypes)/**/**') );
+  // Delete the directories of the example content
+  await del([
+    'src/patterns/pattern-template',
+    'src/prototypes/prototype-example',
+  ]);
 
-  await del(['src/@(patterns|prototypes)/!(index.hbs)', '!public/assets']);
+  // These files are imported into our bundles. We need remove those import
+  // statements. We do this by replacing the import lines in those bundles
+  await replace({
+    files: 'src/scripts/toolkit.js',
+    from: "import '../patterns/pattern-template/pattern-template.js';",
+    to: "",
+  });
+  await replace({
+    files: 'src/styles/toolkit.scss',
+    from: '@import "../patterns/pattern-template/pattern-template.scss";',
+    to: "",
+  });
 
   cb();
 });
