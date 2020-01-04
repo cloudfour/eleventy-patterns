d715849892aefc8e287fab70bc9e4ef96575b9ea
Tyler Sticka <tyler@cloudfour.com>
2020-01-03 16:46:24 -0800

    Simplify dummy image helper (#33)
    * Simplify placeholder helper
    
    * Revert to previous syntax for universal shortcode

--- helpers/shortcode/dummy-image.js
+++ helpers/shortcode/dummy-image.js
@@ -1,31 +1,4 @@
-function template({
-  width,
-  height = width,
-  text = `${width}&#215;${height}`,
-  fontFamily = 'sans-serif',
-  fontWeight = 'bold',
-  fontSize = Math.floor(Math.min(width, height) * 0.2),
-  textAdjust = Math.floor(fontSize * 0.4),
-  bgColor = '#ddd',
-  textColor = 'rgba(0,0,0,0.5)'
-} = {}) {
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}">
-    <rect fill="${bgColor}" width="${width}" height="${height}"/>
-    <text fill="${textColor}" font-family="${fontFamily}" font-size="${fontSize}" dy="${textAdjust}" font-weight="${fontWeight}" x="50%" y="50%" text-anchor="middle">${text}</text>
-  </svg>`;
-}
-
-function svgSourceToDataUri(rendered) {
-  // Thanks to: filamentgroup/directory-encoder
-  const cleaned = rendered
-    .replace(/[\n\r]/gim, '') // Strip newlines
-    .replace(/\t/gim, ' ') // Replace tabs with strings
-    .replace(/'/gim, '\\i'); // Normalize quotes
-  const encoded = encodeURIComponent(cleaned)
-    .replace(/\(/g, '%28') // Encode brackets
-    .replace(/\)/g, '%29');
-  return 'data:image/svg+xml;charset=US-ASCII,' + encoded;
-}
+const simpleSvgPlaceholder = require('@cloudfour/simple-svg-placeholder');
 
 /**
  * Return an inline SVG as a string suitable for including in an `<img>`
@@ -35,7 +8,7 @@ function svgSourceToDataUri(rendered) {
  * @param {Number} [height] - Will default to the same number as width.
  * @param {String} [text] - Will default to the width and height.
  * @return {String}
- * @todo Consider breaking this into a Sindre Sorhus-style micro library?
+ * @see https://github.com/cloudfour/simple-svg-placeholder
  */
 module.exports = (width, height, text) => {
   /**
@@ -51,5 +24,5 @@ module.exports = (width, height, text) => {
     text: typeof text === 'string' ? text : undefined
   };
 
-  return svgSourceToDataUri(template(options));
+  return simpleSvgPlaceholder(options);
 };
