f07a8f4ee75d7d8c3231ca9de6369c9ca475a7f3
Danielle Romo <danielle@cloudfour.com>
2020-01-21 12:34:43 -0800

    Update navigation structure

--- src/scripts/eleventy-scripts/eleventy.js
+++ src/scripts/eleventy-scripts/eleventy.js
@@ -3,7 +3,9 @@ function initMenuToggle() {
   const menu = document.querySelector('.EP_nav__menu');
 
   menuToggle.addEventListener('click', () => {
+    let expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
     menu.classList.toggle('is-open');
+    menuToggle.setAttribute('aria-expanded', !expanded);
   });
 }
 
@@ -11,11 +13,14 @@ function initSubmenuToggle() {
   const submenuToggle = document.querySelectorAll('.EP_nav__submenu-toggle');
 
   submenuToggle.forEach(toggle => {
-    toggle.addEventListener('click', () => {
+    toggle.addEventListener('click', e => {
+      e.preventDefault();
       let target = toggle.nextElementSibling;
+      let expanded = toggle.getAttribute('aria-expanded') === 'true' || false;
       const toggleIcon = toggle.querySelector('.toggle-icon');
       target.classList.toggle('is-open');
       toggleIcon.classList.toggle('is-open');
+      toggle.setAttribute('aria-expanded', !expanded);
     });
   });
 }
