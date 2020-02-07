ba5f5a64696f9f9ad8be5296fdd1332b7649a1b8
Paul Hebert <paul@cloudfour.com>
2020-02-07 10:59:43 -0800

    Use `const` over `let` and minor code reorg

--- src/scripts/eleventy-patterns/navigation.js
+++ src/scripts/eleventy-patterns/navigation.js
@@ -6,10 +6,12 @@ function initMenuToggle() {
   const menu = document.querySelector('.EP_js-nav__menu');
 
   menuToggle.addEventListener('click', () => {
-    let expanded = menuToggle.getAttribute('aria-expanded') === 'true';
-    menu.classList.toggle('is-open');
+    const expanded = menuToggle.getAttribute('aria-expanded') === 'true';
+
     // Toggle `aria-expanded` to the opposite of its current value
     menuToggle.setAttribute('aria-expanded', !expanded);
+
+    menu.classList.toggle('is-open');
     document.body.classList.toggle('EP_no-scroll');
   });
 }
@@ -20,13 +22,16 @@ function initSubmenuToggle() {
 
   submenuToggle.forEach(toggle => {
     toggle.addEventListener('click', event => {
-      let target = toggle.nextElementSibling;
-      let expanded = toggle.getAttribute('aria-expanded') === 'true';
+      const target = toggle.nextElementSibling;
       const toggleIcon = toggle.querySelector('.EP_toggle-icon');
-      target.classList.toggle('is-open');
-      toggleIcon.classList.toggle('is-open');
+
+      const expanded = toggle.getAttribute('aria-expanded') === 'true';
+
       // Toggle `aria-expanded` to the opposite of its current value
       toggle.setAttribute('aria-expanded', !expanded);
+
+      target.classList.toggle('is-open');
+      toggleIcon.classList.toggle('is-open');
     });
   });
 }
